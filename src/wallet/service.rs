d9364d398f74f9f2788c9209dee8269a870b1edd
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-09-19 13:38:01 +0100

    update wallet API's

--- src/wallet/service.rs
+++ src/wallet/service.rs
@@ -2,8 +2,9 @@ use crate::authentication::claims::Claims;
 use crate::errors::RepositoryError::RecordNotFound;
 use crate::errors::ServiceError;
 use crate::errors::ServiceError::RepositoryError;
+use crate::utils::{PaginatedResponse, PaginationParams};
 use crate::wallet::adapters::CreateWalletRequest;
-use crate::wallet::entities::{PaginatedWallet, Wallet};
+use crate::wallet::entities::Wallet;
 use crate::wallet::repository::{WalletRepository, WalletRepositoryExt};
 use sqlx::PgPool;
 use uuid::Uuid;
@@ -36,9 +37,9 @@ pub trait WalletServiceExt: Send + Sync + 'static {
 
     fn fetch_all_wallets(
         &self,
-
         claims: &Claims,
-    ) -> impl std::future::Future<Output = Result<PaginatedWallet, ServiceError>> + Send;
+        pagination_params: &PaginationParams,
+    ) -> impl std::future::Future<Output = Result<PaginatedResponse<Wallet>, ServiceError>> + Send;
 }
 
 impl WalletServiceExt for WalletService {
@@ -53,10 +54,6 @@ impl WalletServiceExt for WalletService {
             .map_err(ServiceError::from)
     }
 
-    async fn fetch_all_wallets(&self, claims: &Claims) -> Result<PaginatedWallet, ServiceError> {
-        todo!()
-    }
-
     async fn fetch_wallet(
         &self,
         claims: &Claims,
@@ -66,5 +63,17 @@ impl WalletServiceExt for WalletService {
             .fetch_wallet(wallet_identifier, &claims.user_identifier)
             .await?
             .ok_or(RepositoryError(RecordNotFound))
+    }
+
+    async fn fetch_all_wallets(
+        &self,
+        claims: &Claims,
+        pagination_params: &PaginationParams,
+    ) -> Result<PaginatedResponse<Wallet>, ServiceError> {
+        let result = self
+            .repository
+            .fetch_all_wallet(&claims.user_identifier, pagination_params)
+            .await?;
+        Ok(result)
     }
 }
