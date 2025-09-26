f0c03ece97f182d1cbe610cbea9d36d208bfb7ce
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-09-26 15:25:36 +0100

    fixes

--- ui/app/middleware/desktop-only.ts
+++ ui/app/middleware/desktop-only.ts
@@ -1,14 +1,14 @@
-import {defineNuxtRouteMiddleware, navigateTo} from '#app'
-import UAParser from 'ua-parser-js'
-
-export default defineNuxtRouteMiddleware((to, from) => {
-    if (process.server) {
-        const headers = useRequestHeaders(['user-agent'])
-        const parser = new UAParser(headers['user-agent'] || '')
-        const deviceType = parser.getDevice().type
-
-        if (deviceType === 'mobile' || deviceType === 'tablet') {
-            return navigateTo('/desktop-only')
-        }
-    }
-})
+// import {defineNuxtRouteMiddleware, navigateTo} from '#app'
+// import UAParser from 'ua-parser-js'
+//
+// export default defineNuxtRouteMiddleware((to, from) => {
+//     if (process.server) {
+//         const headers = useRequestHeaders(['user-agent'])
+//         const parser = new UAParser(headers['user-agent'] || '')
+//         const deviceType = parser.getDevice().type
+//
+//         if (deviceType === 'mobile' || deviceType === 'tablet') {
+//             return navigateTo('/desktop-only')
+//         }
+//     }
+// })
