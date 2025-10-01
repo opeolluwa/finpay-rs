f865fb2cc32c468d16bf07d9995b7701518594ff
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-10-01 18:12:48 +0100

    create curreccy/country service

---
+++ midgard/src/entities.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct Currency {
+    pub identifier: Uuid,
+    pub currency_code: String,
+    pub currency: String,
+    pub country: String,
+    pub flag: Option<String>,
+}
---
+++ midgard/src/mod.rs
@@ -0,0 +1,7 @@
+pub mod entities;
+pub mod handlers;
+pub mod repository;
+
+pub mod service;
+pub mod router;
+mod middleware;
