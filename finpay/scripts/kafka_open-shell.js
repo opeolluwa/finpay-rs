56f63c09ded36e518acf586c888fd3769f537ab4
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-10-01 18:35:22 +0100

    update seeder

--- finpay/scripts/kafka_open-shell.js
+++
@@ -1,8 +0,0 @@
-import {spawn} from 'node:child_process';
-
-const ls = spawn('ls', ['-lh', '/usr']);
-const command = spawn('docker', ['exec', '--workdir', '/opt/kafka/bin/', '-it', 'broker', 'sh'], {stdio: 'inherit'})
-
-command.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
